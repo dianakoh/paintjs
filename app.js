b62f7ff5a287dadf329c7ae41fccc3fba9597c95
dianakoh <dianakoh94@gmail.com>
2020-05-14 17:41:16 +0900

    made a function for changing color

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 600;
 canvas.height = 600;
@@ -31,8 +32,9 @@ function onMouseMove(event) {
 
 }
 
-function onMouseDown(event) {
-    painting = true;
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
 
 if (canvas) {
@@ -41,3 +43,7 @@ if (canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => 
+    color.addEventListener("click", handleColorClick)
+);
